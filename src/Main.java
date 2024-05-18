38c3df3c5cd0ee460f9056dd18d2e8d36360aa47
Gizoskos <gizemgultoprak2004@gmail.com>
2024-05-19 00:23:17 +0300

    main düzeltildi

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,13 @@ public class Main {
         SeleniumApsUtil sel = new SeleniumApsUtil(AutomatedTest.driver);
         // Test senaryosunu başlatmak için setUp() metodunu çağırın
         boolean test1Passed = true;
+        boolean test2Passed = true;
 
         // Test senaryosunu çalıştırın
+
         try {
-            //sel.goToStatisticsList();
-            testFilterGermanyStatistics testFilterGermanyStatistics = new testFilterGermanyStatistics();
-            testFilterGermanyStatistics.testFilterGermanyStatistics();
+        TestingReachAtUsLink testingReachAtUsLink = new TestingReachAtUsLink();
+        testingReachAtUsLink.testLink("user.mackolik.com/UserPages/destek.aspx");
         } catch (AssertionError e) {
             test1Passed = false;
             System.out.println("Test failed: " + e.getMessage());
@@ -24,14 +25,32 @@ public class Main {
             test1Passed = false;
             e.printStackTrace();
             // Örnek bir test senaryos
+        }
+        try{
+            testFilterGermanyStatistics testFilterGermanyStatistics = new testFilterGermanyStatistics();
+            testFilterGermanyStatistics.testFilterGermanyStatistics();
+        } catch (AssertionError e) {
+            test2Passed = false;
+            System.out.println("Test failed: " + e.getMessage());
+        }
+        catch (Exception e) {
+            test2Passed = false;
+            e.printStackTrace();
+            // Örnek bir test senaryos
         } finally {
             // Test senaryosunu sonlandırmak için tearDown() metodunu çağırın
             automatedTest.tearDown();
         }
         if (test1Passed) {
-            System.out.println("All tests passed successfully.");
+            System.out.println("test 1 passed successfully.");
         } else {
-            System.out.println("Some tests failed.");
+            System.out.println("test 1 tests failed.");
         }
+        if (test2Passed) {
+            System.out.println("test 2 passed successfully.");
+        } else {
+            System.out.println("test 2 tests failed.");
+        }
+
     }
 }
